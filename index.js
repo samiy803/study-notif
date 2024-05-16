762f8bd11250bc5fffc356c69e9c6a103b07f306
Sami Yousef <mail@samiyousef.ca>
2024-05-15 23:49:31 -0400

    set correct flags

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function checkForStudies(page) {
 }
 
 async function puppeteerCheck() {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: process.env.NODE_ENV !== "production", args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.waitForNetworkIdle
     page.setViewport({ width: 1920, height: 1080 });
